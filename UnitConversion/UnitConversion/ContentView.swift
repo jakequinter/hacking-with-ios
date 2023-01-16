2f75af5203e27586589f9cf65e7286acc137327d
Jake Quinter <hello@jakequinter.io>
2023-01-16 10:50:14 -0500

    unit converter

---
+++ UnitConversion/UnitConversion/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Jake Quinter on 1/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var temperature = 0.0
+    @State private var startingUnit = "°F"
+    @State private var endingUnit = "°C"
+    
+    let units = ["°F", "°C", "°K"]
+    
+    var fahrenheit: Double {
+        if startingUnit == "°F" {
+            return temperature
+        } else if startingUnit == "°C" {
+            return (temperature * 9/5) + 32
+        } else {
+            return (temperature - 273.15) * 9/5 + 32
+        }
+    }
+    
+    var celsius: Double {
+        (fahrenheit - 32) * 5/9
+    }
+    
+    var kelvin: Double {
+        (fahrenheit - 32) * 5/9 + 273.15
+    }
+    
+    var result: Double {
+        if startingUnit == endingUnit {
+            return temperature
+        } else if endingUnit == "°F" {
+            return fahrenheit
+        } else if endingUnit == "°C" {
+            return celsius
+        } else {
+            return kelvin
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("Starting unit") {
+                    TextField("Temperature", value: $temperature, format: .number)
+                }
+                
+                Section("Conversion") {
+                    Picker("From", selection: $startingUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    Picker("To", selection: $endingUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Result") {
+                    Text("\(result, specifier: "%.2f")\(endingUnit)")
+                }
+            }
+            .navigationTitle("Unit Converter")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
