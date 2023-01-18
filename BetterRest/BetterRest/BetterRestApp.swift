102e58319da8e3a1f34eb16aa94caa45569df8df
Jake Quinter <hello@jakequinter.io>
2023-01-18 18:14:23 -0500

    BetterRest CoreML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jake Quinter on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
