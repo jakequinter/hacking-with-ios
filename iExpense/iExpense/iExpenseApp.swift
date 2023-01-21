52480ab3763d2d1398be2fb32f1656013ae0a55b
Jake Quinter <hello@jakequinter.io>
2023-01-21 12:33:19 -0500

    iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jake Quinter on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
