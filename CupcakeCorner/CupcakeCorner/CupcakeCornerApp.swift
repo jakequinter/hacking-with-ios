078197603f4df90e4986a05eab77c4244f98ebea
Jake Quinter <hello@jakequinter.io>
2023-01-22 14:05:25 -0500

    CupcakeCorner — data

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jake Quinter on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
